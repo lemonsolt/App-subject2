f936f25bd073a1d5210a3c6f3973807216b7daec
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-06-09 12:15:20 +0000

    [update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :lists
-  get 'books' => 'lists#index', as: 'index'
-  post 'lists' => 'lists#create'
-  get 'books/:id/edit'=> 'lists#edit',as: 'edit'
   root 'homes#top'
-  get 'books/:id' => 'lists#show', as: 'book'
-  patch 'books/:id' => 'lists#update', as: 'update_list'
-  delete 'books/:id' => 'lists#destroy', as: 'destroy'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
